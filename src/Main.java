bba366433530c187da9e5980c9168863754183c9
justin-nr <justin98025@gmail.com>
2025-09-03 14:28:54 -0700

    added a . before println

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 System.out.println("HI JUSTIN");
 System.out.println("yo");
-System.outprintln("adada");
+System.out.println("adada");
 System.out.println("stand ready for my arrival");
         }
     }
