089b7ac0c4820252de3796fedf89c315f1d65101
7maken <jakobkeegan1@gmail.com>
2025-09-07 14:36:37 -0700

    started dice simulator code

---
+++ src/Dice.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Dice
+{
+    private static final Scanner in = new Scanner(System.in);
+    private static final Random rng = new Random();
+
+    public static void main(String[] args)
+    {
+        System.out.println("Dice Simulator 🎲");
+
+
+
+
+    }
+}
